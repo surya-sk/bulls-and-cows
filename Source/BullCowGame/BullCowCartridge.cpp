bf77cd80ace908a427d3c564f7b5e74546bfc8a5
Surya Kashyap <surya.sk05@outlook.com>
2020-12-21 11:51:16 -0400

    Create an endgame

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
 // When the game starts
@@ -6,29 +5,33 @@ void UBullCowCartridge::BeginPlay()
 {
     Super::BeginPlay();
     SetupGame();
-    PrintLine(TEXT("Welcome to Bulls and Cows!"));
-    PrintLine(TEXT("Guess the %d letter word."), WordToGuess.Len()); //hard-coded. Change later
-    PrintLine(TEXT("Press ENTER to continue"));
 }
 
 // When the player hits enter
 void UBullCowCartridge::OnInput(const FString& Input) 
 {
-    ClearScreen();
-    if(Input == WordToGuess)
+    if(bGameOver)
     {
-        PrintLine(TEXT("You have guess it! Congratulations"));
-        return;
+        ClearScreen();
+        SetupGame();
     }
     else
     {
-        if(Input.Len() != WordToGuess.Len())
+        if(Input == WordToGuess)
         {
-            PrintLine(TEXT("The word is %d letters long. Try again"), WordToGuess.Len());
+            PrintLine(TEXT("You have guess it! Congratulations"));
+            return;
         }
         else
         {
-            PrintLine(TEXT("Wrong word. Please try again"));
+            if(Input.Len() != WordToGuess.Len())
+            {
+                PrintLine(TEXT("The word is %d letters long. Try again"), WordToGuess.Len());
+            }
+            else
+            {
+                PrintLine(TEXT("Wrong word. Please try again"));
+            }
         }
     }
 }
@@ -38,4 +41,16 @@ void UBullCowCartridge::SetupGame()
 {
     WordToGuess = TEXT("action");
     Lives = 3;
+    bGameOver = false;
+
+    //Print welcome messages
+    PrintLine(TEXT("Welcome to Bulls and Cows!"));
+    PrintLine(TEXT("Guess the %d letter word."), WordToGuess.Len());
+    PrintLine(TEXT("Press ENTER to continue"));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    SetupGame();
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -16,9 +16,11 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
 	void SetupGame();
+	void EndGame();
 
 	// Your declarations go below!
 	private:
 	FString WordToGuess;
 	int32 Lives;
+	bool bGameOver;
 };
