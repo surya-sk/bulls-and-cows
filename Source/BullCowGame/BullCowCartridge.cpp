72d96afceeb35e12c19d626c0318773351bcc715
Surya Kashyap <surya.sk05@outlook.com>
2020-12-21 10:40:37 -0400

    Check if the word is correct or wrong

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,6 +5,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bulls and Cows!"));
+    PrintLine(TEXT("Guess the 5 letter word.")); //hard-coded. Change later
     PrintLine(TEXT("Press ENTER to continue"));
 }
 
@@ -12,5 +13,14 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 {
     ClearScreen();
     FString HiddenWord = TEXT("action");
-    PrintLine(Input);
+    if(Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have guess it! Congratulations"));
+        return;
+    }
+    else
+    {
+        PrintLine(TEXT("Wrong"));
+    }
+    
 }
